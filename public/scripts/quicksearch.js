fbe4e86488551fb75e60e26c88d454a6f207b415
Ben Barber <barberboy@gmail.com>
2014-10-16 12:14:49 +0000

    Renamed simplesearch to quicksearch

--- public/scripts/quicksearch.js
+++ public/scripts/quicksearch.js
@@ -1,4 +1,4 @@
-(function initSearch() {
+(function initQuickSearch() {
     // Show an element
     function show(element) {
         element.classList.remove('hide');
@@ -13,18 +13,20 @@
 
     // Find the bookmark elements
     var bookmarks = [].slice.call(document.querySelectorAll('.bookmark-container > .bookmark'));
-    var searchbox = document.querySelector('.search input');
+    var searchbox = document.querySelector('.quicksearch input');
 
     if (searchbox) {
         // Filter the items on every key press
         searchbox.onkeyup = function() {
             var value = this.value;
             bookmarks
+                // Show everything,
                 .map(show)
+                // Find just the items that *don't* match the search text,
                 .filter(function(item) {
-                    // Find the items that *don't* match the search text.
                     return item.textContent.toLowerCase().indexOf(value.toLowerCase()) === -1;
                 })
+                // And hide them.
                 .forEach(hide);
         };
     }
